fcadde73641f19e31c18efe26cb9fdc986d68496
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-04 11:43:41 +0100

    animation on labeltext

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,14 @@
 import styles from "./styles/Input.module.css";
+import { useState } from "react";
 
 function Input({ type, name, placeholder, id, labelText, ...whatever }) {
+  const [isFocused, setFocused] = useState(false);
+  console.log(isFocused);
   return (
     <>
       <label className={styles.label} htmlFor={id} {...whatever}>
-        <input className={styles.input} type={type} name={name} id={id} {...whatever} />
-        <div className={styles.labelText}>{labelText}</div>
+        <input onFocus={() => setFocused((prev) => true)} onBlur={(e) => setFocused(e.target.value.trim() !== "")} className={styles.input} type={type} name={name} id={id} {...whatever} />
+        <div className={isFocused ? styles.focused + " " + styles.labelText : styles.labelText}>{labelText}</div>
       </label>
     </>
   );
