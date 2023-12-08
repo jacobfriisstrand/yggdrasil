ecf00d9931e00ed134d3343b552e007603e210aa
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-08 15:54:38 +0100

    localhost redone

--- src/app/bands/[slug]/page.jsx
+++ src/app/bands/[slug]/page.jsx
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import Image from "next/image";
 
 export async function generateStaticParams() {
-  const res = await fetch("http://https://funky-melodious-jingle.glitch.me/bands");
+  const res = await fetch("http://localhost:8080/bands");
   const pages = await res.json();
 
   const paths = pages.map((page) => {
@@ -14,7 +14,7 @@ export async function generateStaticParams() {
 
 async function BandPage({ params }) {
   const { slug } = params;
-  const res = await fetch(`http://https://funky-melodious-jingle.glitch.me/bands/${slug}`);
+  const res = await fetch(`http://localhost:8080/bands/${slug}`);
 
   const band = await res.json();
 
@@ -24,8 +24,8 @@ async function BandPage({ params }) {
   // Tjek om billedstiens kilde starter med "https"
   const checkLogoPath = band.logo.startsWith("https");
 
-  // Hvis ikke, tilføj "https://funky-melodious-jingle.glitch.me/" før billedestien
-  const imagePath = checkLogoPath ? band.logo : `http://https://funky-melodious-jingle.glitch.me/${band.logo}`;
+  // Hvis ikke, tilføj "localhost:8080/" før billedestien
+  const imagePath = checkLogoPath ? band.logo : `http://localhost:8080/${band.logo}`;
 
   console.log(band);
   return (
--- src/app/schedule/page.jsx
+++ src/app/schedule/page.jsx
@@ -1,8 +1,8 @@
 import WrapperSchedule from "@/components/WrapperSchedule";
 
 async function Schedule() {
-  const bands = await fetch("http://https://funky-melodious-jingle.glitch.me/bands/").then((res) => res.json());
-  const schedule = await fetch("http://https://funky-melodious-jingle.glitch.me/schedule/").then((res) => res.json());
+  const bands = await fetch("http://localhost:8080/bands/").then((res) => res.json());
+  const schedule = await fetch("http://localhost:8080/schedule/").then((res) => res.json());
 
   return (
     <div>
