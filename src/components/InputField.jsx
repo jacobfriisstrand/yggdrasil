31eed90a9e896175911e65c404727fa4164ff9c1
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-12 14:21:58 +0100

    input field styling ongoing

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,10 +4,28 @@ function InputField({ type, name, placeholder, id, labelText, ...whatever }) {
   const [isFocused, setFocused] = useState(false);
   return (
     <>
-      <label htmlFor={id} {...whatever} className="">
+      <label htmlFor={id} {...whatever} className="grid w-fit items-center">
         {/* //TODO: Fix at autocompleted inhold i felterne ikke udløser animation */}
-        <input onFocus={() => setFocused((prev) => true)} onBlur={(e) => setFocused(e.target.value.trim() !== "")} type={type} name={name} id={id} {...whatever} />
-        <div className={isFocused ? `bg-red-400` + " " + `bg-green-500` : `bg-green-500`}>{labelText}</div>
+        <input
+          className="p-4 [grid-area:1/1]"
+          onFocus={() => setFocused((prev) => true)}
+          onBlur={(e) => setFocused(e.target.value.trim() !== "")}
+          type={type}
+          name={name}
+          id={id}
+          {...whatever}
+        />
+        <div
+          className={
+            isFocused
+              ? `translate-x-[-20px] translate-y-[-20px] scale-75 transition-all duration-100` +
+                " " +
+                `[grid-area:1/1]`
+              : `p-4 transition-all duration-100 [grid-area:1/1]`
+          }
+        >
+          {labelText}
+        </div>
       </label>
     </>
   );
