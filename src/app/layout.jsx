582a9eea1d7d443c2695b0e8c8bb2d58d73b1056
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-17 18:34:27 +0100

    favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full w-full">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className="mx-4 grid h-full grid-rows-[auto_1fr_auto] overflow-x-hidden bg-background-light font-body md:mx-24">
         <header>
           <NavBar />
