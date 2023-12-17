df105541992d6074fd8c746bee311c0ef5392c20
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-17 18:09:39 +0100

    bands

--- src/components/MidBands.jsx
+++ src/components/MidBands.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 function MidBands(props) {
   return (
-    <div className="my-10 flex flex-row flex-wrap justify-around gap-4 text-2xl text-text-light  md:text-4xl ">
+    <div className="my-10 flex flex-row flex-wrap justify-around gap-4 md:gap-10 text-2xl text-text-light  md:text-4xl ">
       {props.bands.slice(11, 30).map((band) => (
         <Link key={band.slug} bands={band.bands} href={`/bands/${band.slug}`}>
-          <h3 className="relative font-subheading transition-all duration-300 before:absolute before:-bottom-2 before:left-0 before:h-1 before:w-0 before:rounded-full before:bg-accent before:opacity-0 before:transition-all  before:duration-500 before:content-[''] hover:before:w-full hover:before:opacity-100 ">
+          <h3 className="relative  text-center font-subheading transition-all duration-300 before:absolute before:-bottom-2 before:left-0 before:h-1 before:w-0 before:rounded-full before:bg-accent before:opacity-0 before:transition-all  before:duration-500 before:content-[''] hover:before:w-full hover:before:opacity-100 ">
             {band.name}
           </h3>
         </Link>
--- src/components/SmallBands.jsx
+++ src/components/SmallBands.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 function SmallBands(props) {
   return (
-    <div className="sm: my-8 grid grid-cols-2 gap-x-2 gap-y-8 uppercase sm:grid-cols-3 sm:gap-x-8   md:text-2xl">
+    <div className="sm: my-8 grid grid-cols-2 gap-x-2 gap-y-8  sm:grid-cols-3 sm:gap-x-8   md:text-2xl">
       {props.bands.slice(31, 124).map((band) => (
         <Link key={band.slug} bands={band.bands} href={`/bands/${band.slug}`}>
-          <h4 className="font-subheading text-accent hover:underline">
+          <h4 className="text-center font-body text-base text-accent hover:underline">
             {band.name}
           </h4>
         </Link>
