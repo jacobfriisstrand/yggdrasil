0b39f42ad90a688e7c106d1fc73a61082ec1515e
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-12-18 06:52:26 +0100

    image optimized

--- src/components/TentCard.jsx
+++ src/components/TentCard.jsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 function TentCard({ children, tentType, imageSrc, tentPrice }) {
   return (
     <div className="space-y-4">
-      <Image className="aspect-square object-cover rounded-sm shadow-md" alt={tentType} src={imageSrc} width={1000} height={1000}></Image>
+      <Image
+        className="aspect-square rounded-sm object-cover shadow-md"
+        alt={tentType}
+        src={imageSrc}
+        quality={10}
+        width={1000}
+        height={1000}
+      ></Image>
       <h3 className="font-subheading text-xl">{tentType}</h3>
       <p>{tentPrice} DKK (tent included)</p>
       {children}
